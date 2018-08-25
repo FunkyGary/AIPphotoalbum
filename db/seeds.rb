f844fece8dca0f36847f6a9e6bc016f621513af6
gary80212@gmail.com <gary80212@gmail.com>
2018-08-25 15:40:18 +0800

    add id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Photo.destroy_all
+
+url = "https://uinames.com/api/?ext&region=england"
+i = 0
+
+15.times do
+  i = i+1
+  response = RestClient.get(url)
+  data = JSON.parse(response.body)
+  photo = Photo.create!(
+    id: i,
+    title: data["name"],
+    date: data["birthday"]["dmy"],
+    description: data["email"],
+    remote_file_location_url: data["photo"]
+  )
+  puts "created No.#{i} photo #{photo.title}"
+end
+
+puts "now you have #{Photo.count} photos data"
